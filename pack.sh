5b1b2358ebe5f42565749fef17d62b79416e4922
Linus Yang <laokongzi@gmail.com>
2017-12-04 20:58:16 +0800

    Add checksum file

--- pack.sh
+++ pack.sh
@@ -26,5 +26,27 @@ zip -9 -r $TARGET . -X -x \*.DS_Store
 cd ../uninstaller
 rm -f ../uninstaller.zip
 zip -9 -r ../uninstaller.zip . -X -x \*.DS_Store
+cd ..
+
+COMMIT=$(git rev-parse --short HEAD)
+echo "AppOpsX Release (build $(date +"%y%m%d")-$COMMIT)" > checksum
+echo "" >> checksum
+echo "MD5 Checksum:" >> checksum
+if hash openssl 2>/dev/null; then
+    MD5="openssl md5 -r"
+else
+    MD5="md5sum"
+fi
+for f in *.zip package/$DEST; do
+    chksum=($($MD5 $f))
+    echo "  ${f##*/}:" >> checksum
+    echo "    $chksum" >> checksum
+done
+if hash perl 2>/dev/null; then
+    perl -pe 's/\r\n|\n|\r/\r\n/g' checksum > checksum.txt
+else
+    sed -e 's/$/\r/' checksum > checksum.txt
+fi
+rm -f checksum
 
 echo 'Done!'
